4258f60b1f77b59357a33e3efa6e9ad7fc4e1a9c
Jared Ye <87924680+JaredYe04@users.noreply.github.com>
2025-01-25 19:49:37 +0800

    q1，以及若干图表

--- 代码/不同运动员服役年限分布.py
+++ 代码/不同运动员服役年限分布.py
@@ -28,24 +28,70 @@ result = result[result['Year'] <= 40]
 
 # 描述统计
 print(result.describe())
+#字体改成Times New Roman
+plt.rcParams['font.sans-serif'] = ['Times New Roman']
+plt.rcParams['axes.unicode_minus'] = False
+
+plt.xticks(fontsize=15)
+plt.yticks(fontsize=15)
+
+
+#网格线加粗，黑色
+plt.grid(linestyle='--', linewidth=0.5, color='black')
+
+
+#坐标系内的背景色变成淡灰色，而不是外部的图表背景色
+plt.gca().set_facecolor('#f0f0f0')
 
 
 #分布拟合，使用核密度估计，绘制核密度图,查看分布情况
-sns.kdeplot(result['Year'], shade=True)
-plt.xlabel('Serve Years')
-plt.ylabel('Density')
-plt.title('Distribution of Serve Years')
+density=sns.kdeplot(result['Year'], shade=True,color='#4c78a8',linewidth=1.5)
+
+
+
+#根据核密度估计，再画一条密度累积曲线，要覆盖面积
+sns.kdeplot(result['Year'], cumulative=True,color='#439894',linewidth=2)
+
+from scipy.signal import find_peaks
+from scipy.stats import gaussian_kde
+# 使用 gaussian_kde 进行核密度估计
+kde = gaussian_kde(result['Year'], bw_method=0.5)  # 可以调整bw_method以改变平滑度
+x_vals = np.linspace(result['Year'].min(), result['Year'].max(), 1000)  # 生成更多的x值
+y_vals = kde(x_vals)  # 计算相应的y值
+# 找到峰值位置
+peaks, _ = find_peaks(y_vals)
+
+# 在每个峰值位置绘制星号
+for peak in peaks:
+    plt.scatter(x_vals[peak], 0.76, color='#e55756', marker='*', s=200, zorder=5)
+
+
+#宽度变大
+plt.xlim(0, 40)
+
+#添加均值线
+plt.axvline(result['Year'].mean(), linestyle='--', linewidth=2,color='#79706e')
+
+#图例
+plt.legend(['Density','Cumulative Density','Peak','Mean'],fontsize=17)
+
+
+plt.xlabel('Serve Years',fontsize=17)
+plt.ylabel('Density',fontsize=17)
+plt.title('Distribution of Serve Years',fontsize=20)
 plt.show()
 
-#验证是否符合帕累托分布，绘制累积分布函数图
-result = result.sort_values('Year')
-result['cumsum'] = np.arange(len(result)) / len(result)
-plt.plot(result['Year'], result['cumsum'])
-plt.xlabel('Serve Years')
-plt.ylabel('Cumulative Probability')
-plt.title('Cumulative Distribution of Serve Years')
 
-plt.show()
+# #验证是否符合帕累托分布，绘制累积分布函数图
+# result = result.sort_values('Year')
+# result['cumsum'] = np.arange(len(result)) / len(result)
+# plt.plot(result['Year'], result['cumsum'])
+# plt.xlabel('Serve Years')
+# plt.ylabel('Cumulative Probability')
+# plt.title('Cumulative Distribution of Serve Years')
+
+
+# plt.show()
 
 
 pass
